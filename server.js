ba82df8e2306250b125762a0cc033c71cb6bf8db
Xevier Turrubiartes <xevierturrubiartes@gmail.com>
2020-12-17 15:57:57 -0600

    still setting up

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.get('/api/*', (req, res) => {
   res.json({ok: true});
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
 
 module.exports = {app};
