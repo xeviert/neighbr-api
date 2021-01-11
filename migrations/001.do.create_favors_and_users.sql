7f910caa6ec2a808763e0467f0a7cead296c31f0
Xevier Turrubiartes <xevierturrubiartes@gmail.com>
2021-01-11 13:18:40 -0600

    ready to deply

--- migrations/001.do.create_favors_and_users.sql
+++ migrations/001.do.create_favors_and_users.sql
@@ -1,8 +1,20 @@
-CREATE TABLE IF NOT EXISTS users (
-    user_id BIGSERIAL NOT NULL PRIMARY KEY,
+CREATE TABLE users (
+    user_id INT GENERATED ALWAYS AS IDENTITY
     first_name VARCHAR(20) NOT NULL,
     last_name VARCHAR(20) NOT NULL,
     email VARCHAR(30) NOT NULL,
-    password VARCHAR(32) NOT NULL,
-    address VARCHAR(50) NOT NULL
+    password VARCHAR(164) NOT NULL,
+    address VARCHAR(50) NOT NULL,
+    PRIMARY KEY(user_id)
+);
+
+CREATE TABLE favors (
+    favor_id INT GENERATED ALWAYS AS IDENTITY,
+    user_id INT,
+    assign_to INT,
+    title VARCHAR(20) NOT NULL,
+    payment VARCHAR(20) NOT NULL,
+    description VARCHAR(300) NOT NULL,
+    posted TIMESTAMP DEFAULT now() NOT NULL,
+    PRIMARY KEY(favor_id),
 )
--- src/config.js
+++ src/config.js
@@ -1,13 +1,12 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    CLIENT_ORIGIN: "http://localhost:8000",
     API_BASE_URL: 
       process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api",
-    TEST_DB_URL:
-      process.env.TEST_DB_URL || "postgresql://postgres@localhost/neighbr-api-test",
-    DB_URL: 
-      process.env.DB_URL || 'postgresql://postgres@localhost/neighbr-api',
+    TEST_DATABASE_URL:
+      process.env.TEST_DATABASE_URL || "postgresql://postgres@localhost/neighbr-api-test",
+    DATABASE_URL: 
+      process.env.DATABASE_URL || 'postgresql://postgres@localhost/neighbr-api',
     JWT_SECRET:
       process.env.JWT_SECRET,
   }
