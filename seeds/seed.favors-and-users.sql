7f722b3e3d640287306744ecc0aad24636783abc
Xevier Turrubiartes <xevierturrubiartes@gmail.com>
2021-01-11 15:37:14 -0600

    heroku

--- seeds/seed.favors-and-users.sql
+++ seeds/seed.favors-and-users.sql
@@ -1,14 +1,14 @@
 BEGIN;
-
+TRUNCATE users;
 INSERT INTO users
     (first_name, last_name, email, password, address)
 VALUES
-    ('Alanis', 'Hand', 'Adriel44@gmail.com', '2_ClvsQTdYyG0tQ', '982 Dietrich Extension'),
-    ('Lindsay', 'Schowalter', 'Britney.Luettgen@gmail.com', 'V4CFEjc5n3cVHJx', '60886 Goldner Lodge'),
-    ('Cornelius', 'Zemlak', 'Adan57@yahoo.com', 'LNaOFqbg5E5m_7j', '806 Joe Harbors'),
-    ('Rahsaan', 'Wintheiser', 'Abagail_Vandervort52@yahoo.com', 'VpYK0CeOgrtoFsh', '1598 Hagenes Flats'),
-    ('Moises', 'Brakus', 'Terence.Ebert25@gmail.com', 'inlN4UmZT3TtTBn', '1827 Bruen Place'),
-    ('Nelda', 'Bradtke', 'Clyde56@gmail.com', '9q9BYceKyajTYGg', '969 Predovic Ridge'),
-    ('Marcel', 'Bauch', 'Cynthia_Brekke@gmail.com', '9VnVDogmze8Ie79', '501 Hermann Tunnel');
+    ('Alanis', 'Hand', 'Adriel44@gmail.com', '$2a$12$jqMHBp1IoIY9/NZvI.KnxudFRxoBY.GoNT/sG2IalkVtULjBwN1HW', '982 Dietrich Extension'),
+    ('Lindsay', 'Schowalter', 'Britney.Luettgen@gmail.com', '$2a$12$jqMHBp1IoIY9/NZvI.KnxudFRxoBY.GoNT/sG2IalkVtULjBwN1HW', '60886 Goldner Lodge'),
+    ('Cornelius', 'Zemlak', 'Adan57@yahoo.com', '$2a$12$jqMHBp1IoIY9/NZvI.KnxudFRxoBY.GoNT/sG2IalkVtULjBwN1HW', '806 Joe Harbors'),
+    ('Rahsaan', 'Wintheiser', 'Abagail_Vandervort52@yahoo.com', '$2a$12$jqMHBp1IoIY9/NZvI.KnxudFRxoBY.GoNT/sG2IalkVtULjBwN1HW', '1598 Hagenes Flats'),
+    ('Moises', 'Brakus', 'Terence.Ebert25@gmail.com', '$2a$12$jqMHBp1IoIY9/NZvI.KnxudFRxoBY.GoNT/sG2IalkVtULjBwN1HW', '1827 Bruen Place'),
+    ('Nelda', 'Bradtke', 'Clyde56@gmail.com', '$2a$12$jqMHBp1IoIY9/NZvI.KnxudFRxoBY.GoNT/sG2IalkVtULjBwN1HW', '969 Predovic Ridge'),
+    ('Marcel', 'Bauch', 'Cynthia_Brekke@gmail.com', '$2a$12$jqMHBp1IoIY9/NZvI.KnxudFRxoBY.GoNT/sG2IalkVtULjBwN1HW', '501 Hermann Tunnel');
 
 COMMIT;
