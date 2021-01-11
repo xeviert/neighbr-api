c782e6191c2856047c4e998a5ba9e11470abc469
Xevier Turrubiartes <xevierturrubiartes@gmail.com>
2021-01-11 13:46:26 -0600

    heroku

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    API_BASE_URL: 
-      process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api",
     TEST_DATABASE_URL:
       process.env.TEST_DATABASE_URL || "postgresql://postgres@localhost/neighbr-api-test",
     DATABASE_URL: 
